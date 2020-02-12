e0df36522223a3990da28dbe66b42ab1ee6f6205
apoorv.singh@gdn-commerce.com <apoorv.singh@gdn-commerce.com>
2020-02-12 14:37:26 +0530

    buttons disabled

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,9 @@ const ATTACK_VALUE = 10;
 const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
+const STRONG_BUTTON_VALUE = 4;
+const HEAL_BUTTON_VALUE = 5;
+
 
 const MODE_ATTACK = 'ATTACK';
 const MODE_STRONG_ATTACK = 'STRONG_ATTACK';
@@ -12,9 +15,13 @@ const LOG_EVENT_PLAYER_HEAL = 'PLAYER_HEAL';
 const LOG_EVENT_GAME_OVER = 'GAME_OVER';
 
 const enteredValue = prompt('Maximum life for you and the monster.', '100');
+document.getElementById("strong-attack-btn").disabled = true;
+document.getElementById('heal-btn').disabled = true;
 
 let chosenMaxLife = parseInt(enteredValue);
 let battleLog = [];
+let attackCount = 0;
+let healCount = 0;
 
 if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
     chosenMaxLife = 100;
@@ -123,14 +130,46 @@ function attackMonster(mode) {
 }
 
 function attackHandler() {
+    if (attackCount >= HEAL_BUTTON_VALUE) {
+        document.getElementById('heal-btn').disabled = false;
+    }
     attackMonster(MODE_ATTACK);
+    attackCount++;
+    healCount++;
+    if (attackCount >= STRONG_BUTTON_VALUE) {
+        document.getElementById("strong-attack-btn").disabled = false;
+
+    }
+    console.log('attack count', attackCount);
+
 }
 
 function strongAttackHandler() {
+    healCount++;
+    if (attackCount >= HEAL_BUTTON_VALUE) {
+        document.getElementById('heal-btn').disabled = false;
+    }
+    console.log('strong attack', attackCount);
+    if (attackCount >= STRONG_BUTTON_VALUE) {
+        console.log('in if');
+        document.getElementById("strong-attack-btn").disabled = false;
+        attackCount = 0;
+        document.getElementById("strong-attack-btn").disabled = true;
+    }
     attackMonster(MODE_STRONG_ATTACK);
+
 }
 
 function healPlayerHandler() {
+
+    if (attackCount >= HEAL_BUTTON_VALUE) {
+        console.log('healing...');
+        document.getElementById('heal-btn').disabled = false;
+        healCount = 0;
+        document.getElementById('heal-btn').disabled = true;
+
+    }
+
     let healValue;
     if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
         alert("You can't heal to more than your max initial health.");
@@ -146,6 +185,7 @@ function healPlayerHandler() {
         currentMonsterHealth,
         currentPlayerHealth
     );
+
     endRound();
 }
 
